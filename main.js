9e90723b7d4ef5e84897adef24872fb06566881b
Nollee <normannmail@gmail.com>
2020-04-21 14:32:03 +0200

    login og welcome

--- main.js
+++ main.js
@@ -5,6 +5,7 @@ import ClientsPage from "./pages/clients.js";
 import ContactPage from "./pages/contact.js";
 import _spaService from "./services/spa.js";
 import QuestionPage from "./pages/questions.js";
+import WelcomePage from "./pages/welcome.js";
 
 // Declare and init
 let navbar = new NavBar();
@@ -13,6 +14,9 @@ let aboutPage = new AboutPage();
 let clientsPage = new ClientsPage();
 let contactPage = new ContactPage();
 let questionPage = new QuestionPage();
+let welcomePage = new WelcomePage();
+
 
 // init services
 _spaService.init();
+
