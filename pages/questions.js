2d67a6843af613b3376d042c52a769e33d62a2b6
Brinkvisual <48533353+Brinkvisual@users.noreply.github.com>
2020-04-21 14:51:02 +0200

    Added basic styling to Question page

--- pages/questions.js
+++ pages/questions.js
@@ -9,7 +9,51 @@ export default class QuestionPage {
         <header class="topbar">
           <h2>questions</h2>
         </header>
+        <section class="question-section">
+          <div class="question-container">
+            <div>
+            <h2 class="question">5. Hvor mange kvæg har du på din gård?</h2>
+
+            <div class="question-indicators">
+              <div class="sub-question-container">
+                <h4 class="sub-question">Hvordan finder jeg svaret?</h4>
+                <img src="./images/questionmark.svg" alt="questionmark">
+              </div>
+
+              <div class="sub-question-container">
+              <h4 class="sub-question">Gennemsnittet er 158</h4>
+              <img src="./images/exclamation.svg" alt="questionmark">
+              </div>
+            </div>
+           
+
+            <form>
+
+              <div>
+                <h3 class="sub-question">5.1 Hvor mange køer har du?</h3>
+                <input type="text" name="cows" placeholder="fx. 120" />
+              </div>
+
+              <div>
+                <h3 class="sub-question">5.2 Hvor mange tyrer har du?</h3>
+                <input type="text" name="cows" placeholder="fx. 120" />
+              </div>
+
+              <div> 
+                <h3 class="sub-question">5.3 Hvor mange kalve har du?</h3>
+                <input type="text" name="cows" placeholder="fx. 120" />
+              </div>
+
+            </form>
+            </div>
+          </div>
+          <div class="question-img">
+            <img src="./images/cow-icon.svg">
+          </div>
+        </section>
+
       </section>
+    
     `;
   }
 }
