c2a568d7c82ecbf3c975c2f62489b5ec06a5884e
Brinkvisual <48533353+Brinkvisual@users.noreply.github.com>
2020-04-21 13:39:19 +0200

    Made basic styling and added background

--- main.js
+++ main.js
@@ -4,14 +4,15 @@ import AboutPage from "./pages/about.js";
 import ClientsPage from "./pages/clients.js";
 import ContactPage from "./pages/contact.js";
 import _spaService from "./services/spa.js";
-
+import QuestionPage from "./pages/questions.js";
 
 // Declare and init
 let navbar = new NavBar();
 let homePage = new HomePage();
 let aboutPage = new AboutPage();
-let clientsPage = new ClientsPage;
+let clientsPage = new ClientsPage();
 let contactPage = new ContactPage();
+let questionPage = new QuestionPage();
 
 // init services
 _spaService.init();
