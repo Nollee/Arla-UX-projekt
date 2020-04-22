9cfdbf179e209cacda39cc700d110581f09f7f5b
Brinkvisual <48533353+Brinkvisual@users.noreply.github.com>
2020-04-22 14:45:11 +0200

    New pages
    -Made prize page
    -Updates to Question page

--- main.js
+++ main.js
@@ -5,10 +5,10 @@ import ClientsPage from "./pages/clients.js";
 import ContactPage from "./pages/contact.js";
 import _spaService from "./services/spa.js";
 import YourPage from "./pages/yourpage.js"; 
-
 import QuestionPage from "./pages/questions.js";
 import WelcomePage from "./pages/welcome.js";
-import HowToPage from "./pages/howto.js"
+import HowToPage from "./pages/howto.js";
+import PrizePage from "./pages/prize.js";
 
 // Declare and init
 let navbar = new NavBar();
@@ -17,13 +17,34 @@ let aboutPage = new AboutPage();
 let clientsPage = new ClientsPage();
 let contactPage = new ContactPage();
 let yourPage = new YourPage();  
- 
 let questionPage = new QuestionPage();
 let welcomePage = new WelcomePage();
 let howtoPage = new HowToPage();
+let prizePage = new PrizePage();
 
 
 
 // init services
 _spaService.init();
+
+
+
+// Question navigation
+
+const closeButton = document.querySelector(".q-close-button");
+const openButton = document.querySelector(".q-open-button");
+
+const overlay = document.querySelector(".overlay");
+
+const list = document.querySelector(".question-list");
+
+closeButton.addEventListener('click', closeList);
+openButton.addEventListener('click', closeList);
+overlay.addEventListener('click', closeList);
+
+
+function closeList(){
+    list.classList.toggle('fade-out');
+    overlay.classList.toggle('overlay-show');
+}
 
