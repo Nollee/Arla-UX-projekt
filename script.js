6c553b46c31f669da536854439e8ce7ac72bdf45
Nollee <normannmail@gmail.com>
2020-04-23 16:26:26 +0200

    firebase

--- script.js
+++ script.js
@@ -1,3 +1,39 @@
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyCpb0OF-Co5wJU3PRvBwWIqtp71yOi25wY",
+    authDomain: "arla-ux.firebaseapp.com",
+    databaseURL: "https://arla-ux.firebaseio.com",
+    projectId: "arla-ux",
+    storageBucket: "arla-ux.appspot.com",
+    messagingSenderId: "333480570558",
+    appId: "1:333480570558:web:ab212ac76f48f0d92ab8ef"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+  const db = firebase.firestore();
+  const userRef = db.collection("users");
+  
+  let selectedUserId = "";
+  
+  // ========== READ ==========
+  // watch the database ref for changes
+  userRef.onSnapshot(function(snapshotData) {
+    let users = [];
+    snapshotData.forEach(function(doc) {
+      let user = doc.data();
+      console.log(user);
+      user.id = doc.id;
+      users.push(user);
+    });
+    appendUsers(users);
+  });
+
+
+
+
+
+
+
 
 function cowData(){
     document.querySelector(".cow-nav").classList.toggle('next');
